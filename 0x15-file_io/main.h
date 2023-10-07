22ca831fed927babc877ff5b12389084dd8a52ae
chapsvictor <adebayovictordare@yahoo.com>
2023-10-07 06:00:18 +0100

    changes

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,9 +1,9 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
 /*
- * File: holberton.h
- * Auth: Brennan D Baraban
+ * File: main.h
+ * Auth: Gedeon Obae Gekonge
  * Desc: Header file containing prototypes for all functions
  *       written in the 0x14-file_io directory.
  */
@@ -17,4 +17,4 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
-#endif /* HOLBERTON_H */
+#endif /* MAIN_H */
